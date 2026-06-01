66984e478883dcfeebeb80a5793fd685d3c75e4a
ItsQifuat <qifuatnamlih@gmail.com>
2026-06-01 07:02:14 +0700

    Update Supabase environment variable references

--- api/get-config.js
+++ api/get-config.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   // Mengambil data dari Environment Variables Vercel
   res.status(200).json({
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_ANON_KEY
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   });
 }
